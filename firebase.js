51a907a59d6fbb6d3b482a8cb16d2cc31ebba5e4
47tcyrgstj-design <47tcyrgstj@privaterelay.appleid.com>
2026-08-17 00:19:00 +0300

    Add Firebase Storage initialization

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAvGTD6Kcyms_tBHDdFB4C8xC5oV-XJdHE",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const storage = getStorage(app);
